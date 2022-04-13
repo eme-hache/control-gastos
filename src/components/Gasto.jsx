b76bfb37a772bf487e41f34e6e9dd414b123ff88
Eme-Hache <luisalfonsodsg@outlook.com>
2022-04-12 20:14:22 -0500

    retoques a modal mobile y gasto mobile

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -56,16 +56,18 @@ const Gasto = ({ gasto, setGastoEditar, eliminarGasto }) => {
                             src={diccionarioIconos[categoria]}
                             alt='Icono gasto'
                         />
-                        <div className="descripcion-gasto">
-                            <p className="categoria">{categoria}</p>
-                            <p className="nombre-gasto">{nombre}</p>
-                            <p className="fecha-gasto">
-                                Agregado el: {''}
-                                <span>{formatearFecha(fecha)}</span>
-                            </p>
+                        <div className='datos'>
+                            <div className="descripcion-gasto">
+                                <p className="categoria">{categoria}</p>
+                                <p className="nombre-gasto">{nombre}</p>
+                                <p className="fecha-gasto">
+                                    Agregado el: {''}
+                                    <span>{formatearFecha(fecha)}</span>
+                                </p>
+                            </div>
+                            <p className="cantidad-gasto">${cantidad}</p>
                         </div>
                     </div>
-                    <p className="cantidad-gasto">${cantidad}</p>
                 </div>
             </SwipeableListItem>
         </SwipeableList>
